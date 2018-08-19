863de571ba6f3e6249bdbf75f28a72e6466221f7
bloeys <bloeys@outlook.com>
2018-08-19 21:37:54 +0200

    Added barrel

---
+++ Assets/_Game/Scripts/Environment/Barrel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Barrel : MonoBehaviour
+{
+    [Range(-20, 20)]
+    float effectRadius, healthEffect, radEffect;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Explode()
+    {
+        var pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
+        if (Vector3.Distance(transform.position, pc.transform.position) <= effectRadius)
+        {
+            //Hurt player
+        }
+
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var e in enemies)
+        {
+            if (Vector3.Distance(transform.position, e.transform.position) <= effectRadius)
+            {
+                //e.GetComponent<BullyController>()
+            }
+        }
+    }
+
+    /// <summary>
+    /// Pass transform of the object that is throwing the barrel
+    /// </summary>
+    /// <param name="tr"></param>
+    public void Throw(Transform tr)
+    {
+
+    }
+}
