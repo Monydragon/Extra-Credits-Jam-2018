fff0dfd46f69e8e142f5be56b7e6c34fd5cad3d5
bloeys <bloeys@outlook.com>
2018-08-19 17:45:41 +0200

    Added credits scene and credits text

---
+++ Assets/_Game/Scripts/Credits/CreditsControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsControl : MonoBehaviour
+{
+    public float speed = 10, stopCreditsYPos = 175;
+    RectTransform rect;
+    
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, rect.anchoredPosition.y + speed * Time.deltaTime);
+
+        if (rect.anchoredPosition.y >= stopCreditsYPos)
+            Application.Quit();
+    }
+}
