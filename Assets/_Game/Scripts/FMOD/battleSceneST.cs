5ef8347be2bd8aff62bd6bde75cd65097d5d4730
bloeys <bloeys@outlook.com>
2018-08-20 07:37:18 +0200

    Sound + Item bug fixes + Bigger arena

--- Assets/_Game/Scripts/FMOD/battleSceneST.cs
+++ Assets/_Game/Scripts/FMOD/battleSceneST.cs
@@ -2,42 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class battleSceneST : MonoBehaviour {
+public class battleSceneST : MonoBehaviour
+{
 
     //UI
     [FMODUnity.EventRef]
-	//"musicEvent" stores event path
-	public string musicEvent;
-	
-	//Event instance
-	FMOD.Studio.EventInstance music;
-	
-	//intensity VARIABLE HERE (substitute number)
-	int intensityPar = 1;
-	
-	public int vol;
+    //"musicEvent" stores event path
+    public string musicEvent;
 
-private void OnEnable()
+    //Event instance
+    FMOD.Studio.EventInstance music;
+
+    //intensity VARIABLE HERE (substitute number)
+    int intensityPar = 1;
+
+    public int vol;
+
+    private void OnEnable()
     {
-		//Instances "music" and enables it
-		music = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
+        //Instances "music" and enables it
+        music = FMODUnity.RuntimeManager.CreateInstance(musicEvent);
         music.start();
-		//Attaches instance to object
-		FMODUnity.RuntimeManager.AttachInstanceToGameObject(music, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        //Attaches instance to object
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(music, GetComponent<Transform>(), GetComponent<Rigidbody>());
 
-		//starts "intensity" as 0
-		music.setParameterValue("intensity", 0);
-		music.setParameterValue("vol", vol);
+        //starts "intensity" as 0
+        music.setParameterValue("intensity", 0);
+        music.setParameterValue("vol", vol);
+    }
+
+    void OnDestroy()
+    {
+        music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        music.release();
     }
 
     private void OnDisable()
     {
-		//Releases "music" resources
-		music.release();
+        //Releases "music" resources
+        music.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        music.release();
     }
-	
-	void Update () {
-		//uncomment following:
-		//intensityPar = new time variable thing <----------
-	}
+
+    void Update()
+    {
+        //uncomment following:
+        //intensityPar = new time variable thing <----------
+        music.setParameterValue("intensity", Time.timeSinceLevelLoad / 10);
+    }
 }
